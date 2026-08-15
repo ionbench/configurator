2fed5709b3c37e4ea9ebce100329cfd4e77c5fc2
ionbench <ionbench.colle@gmail.com>
2026-08-16 00:00:28 +0200

    Update ne27.js

--- ne27.js
+++ ne27.js
@@ -1271,14 +1271,17 @@ $('#save_b').click(function() {//Sauvegarde des données par email
                                             
                             myConfigResult.nameOption.push(nameOption);
                             myConfigResult.desOption.push('- ' + nameWithDescription);
-					});	
+						});	
 					$('#modalSave').modal('show');
-				}
-				$('#save_b').prop("disabled",false);
-			});
-		}else{
-			$('#save_b').prop("disabled",false);
-		}
+                    $('#save_b').prop("disabled", false);
+				});
+                } else {
+                    $('#save_b').prop("disabled", false);
+                }
+            });
+        } else {
+            $('#save_b').prop("disabled", false);
+        }
 	});
 });
 $('#continue_b').click(function() {	
