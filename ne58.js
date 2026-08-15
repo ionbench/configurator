5396e04e460c2fd43afa4045e4d3e05fea65e775
ionbench <ionbench.colle@gmail.com>
2026-08-15 23:58:28 +0200

    Update ne58.js

--- ne58.js
+++ ne58.js
@@ -1294,12 +1294,15 @@ $('#save_b').click(function() {//Sauvegarde des données par email
                             myConfigResult.desOption.push('- ' + nameWithDescription);
 					});	
 					$('#modalSave').modal('show');
-				}
-				$('#save_b').prop("disabled",false);
-			});
-		}else{
-			$('#save_b').prop("disabled",false);
-		}
+                    $('#save_b').prop("disabled", false);
+				});
+                } else {
+                    $('#save_b').prop("disabled", false);
+                }
+            });
+        } else {
+            $('#save_b').prop("disabled", false);
+        }
 	});
 });
 $('#continue_b').click(function() {	
